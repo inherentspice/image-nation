c1a911716139610ae5ebcef21c15296280c4272c
Stephen Smith <stephenlehanesmith@gmail.com>
2023-01-01 18:16:11 +0800

    created component to display generated images

--- src/services/image-creation-service.tsx
+++ src/services/image-creation-service.tsx
@@ -1,14 +1,16 @@
 import axios from "axios";
+import { AxiosResponse } from "axios";
+import { ImageData } from "../types/types";
 const baseUrl = "/api/image";
 
 interface generateImageObject {
-  imageDescription: String,
-  imageSize: String,
+  imageDescription: string,
+  imageSize: string,
 }
 
 
-const generateImage = (newObject: generateImageObject): Promise<String> => {
-  return axios.get(`${baseUrl}?imageDescription=${newObject.imageDescription}&imageSize=${newObject.imageSize}`)
+const generateImage = (newObject: generateImageObject): Promise<AxiosResponse<ImageData>> => {
+  return axios.get<ImageData>(`${baseUrl}?imageDescription=${newObject.imageDescription}&imageSize=${newObject.imageSize}`)
 }
 
 export default {
---
+++ src/types/types.tsx
@@ -0,0 +1,9 @@
+export interface ImageData {
+  data: Object,
+  url: string,
+  status: Number,
+}
+
+export interface ImageDisplayProps {
+  imageURL: string,
+}
